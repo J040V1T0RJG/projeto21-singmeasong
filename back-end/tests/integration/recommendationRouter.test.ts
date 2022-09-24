4feb9cfa7c09d02b286f7b1701b1fe43ab2c2644
J040V1T0R <joaovitor.jovg73@gmail.com>
2022-09-24 09:35:14 -0300

    test: test router GET '/recommendations

--- back-end/tests/integration/recommendationRouter.test.ts
+++ back-end/tests/integration/recommendationRouter.test.ts
@@ -1,9 +1,18 @@
 import supertest from "supertest";
 import app from "../../src/app";
+import { prisma } from "../../src/database";
 import { recommendationFactory } from "../factories/recommendation";
 
+beforeEach(async () => {
+    await prisma.$executeRaw`TRUNCATE TABLE recommendations`;
+});
+
+afterAll(async () => {
+    await prisma.$disconnect();
+});
+
 describe("Test POST '/recommendations'", () => {
-    it("Should return statusCode 201 if the recommendation is successfully registered", async () => {
+    it("Should return statusCode 201, if the recommendation is successfully registered", async () => {
         const recommendation = recommendationFactory();
 
         const result = await supertest(app).post("/recommendations").send(recommendation);
@@ -29,5 +38,19 @@ describe("Test POST '/recommendations'", () => {
         const result = await supertest(app).post("/recommendations").send(recommendation);
 
         expect(result.status).toBe(409);
+    });
+});
+
+describe("Test GET '/recommendations'", () => {
+    it("Must return an array of objects", async () => {
+        const recommendation = recommendationFactory();
+
+        await supertest(app).post("/recommendations").send(recommendation);
+        const result = await supertest(app).get("/recommendations").send();
+
+        expect(result.body).toBeDefined();
+        expect(result.body).toBeInstanceOf(Array);
+        expect(result.body[0]).toBeInstanceOf(Object);
+        expect(result.body.length).toBeGreaterThan(0);
     });
 });
