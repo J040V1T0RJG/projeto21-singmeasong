5b7b2e614885863e4d6a16167e53da09dea35e06
J040V1T0R <joaovitor.jovg73@gmail.com>
2022-09-26 16:55:14 -0300

    test: recommendationService => function up/downvote()

--- back-end/tests/unit/recommendation.test.ts
+++ back-end/tests/unit/recommendation.test.ts
@@ -1,6 +1,8 @@
+import { Recommendation } from "@prisma/client";
 import { recommendationRepository } from "../../src/repositories/recommendationRepository";
 import { CreateRecommendationData, recommendationService } from "../../src/services/recommendationsService";
-import { recommendationFactory } from "../factories/recommendation";
+import { recommendationFactory, recommendationWithScoreFactory } from "../factories/recommendation";
+import { conflictError, notFoundError } from "../../src/utils/errorUtils";
 
 beforeEach(() => {
     jest.resetAllMocks();
@@ -35,10 +37,88 @@ describe("Recommendation Services unit tests", () => {
 
         const promise = recommendationService.insert(recommendation);
 
-        expect(promise).rejects.toEqual({
-            type: "conflict",
-            message: "Recommendations names must be unique"
-        });
+        expect(promise).rejects.toEqual(conflictError("Recommendations names must be unique"));
         expect(recommendationRepository.create).not.toBeCalled();
     });
+
+    it("Deve retornar uma recomendação aleatoria", async () => {
+        jest
+            .spyOn(recommendationRepository, "findAll")
+            .mockImplementationOnce((): any => {
+                const recommendations = [];
+
+                for(let i = 0; i < 10; i++) {
+                    const recommendation = recommendationWithScoreFactory();
+                    recommendations.push(recommendation);
+                };
+
+                return recommendations;
+            });
+
+        const result = await recommendationService.getRandom();
+    
+        expect(result).toBeInstanceOf(Object);
+    });
+
+    it("should increment score if id exists", async () => {
+        const recommendation = {id: 1, ...recommendationFactory()};
+        const id: number = recommendation.id;
+    
+        jest
+          .spyOn(recommendationRepository, "find")
+          .mockImplementationOnce((): any => {
+            return recommendation;
+          });
+
+        jest
+          .spyOn(recommendationRepository, "updateScore")
+          .mockImplementationOnce((): any => {
+            return recommendation;
+          });
+    
+        await recommendationService.upvote(id);
+
+        expect(recommendationRepository.find).toBeCalled();
+        expect(recommendationRepository.updateScore).toBeCalled();
+      });
+
+      it("should not increment score if id doesn't existis", async () => {
+        const recommendation = {id: 1, ...recommendationFactory()};
+        const id: number = recommendation.id;
+    
+        jest
+            .spyOn(recommendationRepository, "find")
+            .mockImplementationOnce((): any => {return null});
+
+        jest
+            .spyOn(recommendationRepository, "updateScore");
+    
+        const promise = recommendationService.upvote(id);
+
+        expect(promise).rejects.toEqual(notFoundError());
+        expect(recommendationRepository.find).toBeCalled();
+        expect(recommendationRepository.updateScore).not.toBeCalled();
+      });
+
+      it("should decrement score if id exists", async () => {
+        const recommendation = {id: 1, ...recommendationFactory()};
+        const id: number = recommendation.id;
+    
+        jest
+          .spyOn(recommendationRepository, "find")
+          .mockImplementationOnce((): any => {
+            return recommendation;
+          });
+
+        jest
+          .spyOn(recommendationRepository, "updateScore")
+          .mockImplementationOnce((): any => {
+            return recommendation;
+          });
+    
+        await recommendationService.downvote(id);
+
+        expect(recommendationRepository.find).toBeCalled();
+        expect(recommendationRepository.updateScore).toBeCalled();
+      });
 });
